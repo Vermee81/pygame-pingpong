df2a8e48504105863c53514ff002afd22db28d9b
Vermee81 <hrksb5029@gmail.com>
2025-05-18 00:32:07 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pygame Pingpong
+
+This is a practice project to get used to Pygame.
+
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from pygame-pingpong!")
+
+
+if __name__ == "__main__":
+    main()
