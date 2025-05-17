f609e74ca196f144cc9917636767f66c57c057c4
Vermee81 <hrksb5029@gmail.com>
2025-05-18 01:08:49 +0900

    初期画面を作成する

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import pygame
+from pygame.locals import QUIT, Rect
+import sys
+
+
 def main():
-    print("Hello from pygame-pingpong!")
+    pygame.init()
+    SCREEN = Rect((0, 0, 400, 600))
+    screen = pygame.display.set_mode(SCREEN.size)
+    pygame.display.set_caption("Let's PingPong")
+
+    clock = pygame.time.Clock()
+
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        clock.tick(60)
 
 
 if __name__ == "__main__":
