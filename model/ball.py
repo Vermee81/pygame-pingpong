5a24f3e4e837ab34604f097618114538cae6f132
Vermee81 <hrksb5029@gmail.com>
2025-05-19 00:03:13 +0900

    :recycle: 衝突判定をテストしやすくするためにリファクタリングする

--- model/ball.py
+++ model/ball.py
@@ -19,41 +19,45 @@ class Ball:
     def start(self):
         self.status = "RUNNING"
 
-    def update(self, SCREEN: pygame.locals.Rect):
-        if self.status == "INIT":
-            self.rect.centerx = (
-                self.bar.rect.centerx
-            )  # ボールのxの初期位置は棒の中心に合わせる
-            self.rect.bottom = (
-                self.bar.rect.top
-            )  # ボールのyの初期位置は棒の上辺に合わせる
-            return
+    def _handle_bar_collision(self, old_rect):
+        if self.bar.rect.left >= old_rect.right:
+            self.rect.right = self.bar.rect.left
+            self.dx = -self.dx
+        elif self.bar.rect.right < old_rect.left:
+            self.rect.left = self.bar.rect.right
+            self.dx = -self.dx
+        elif self.bar.rect.top >= old_rect.bottom:
+            self.rect.bottom = self.bar.rect.top
+            x = self.rect.centerx - self.bar.rect.left
+            y = (45 - 135) * x / self.bar.rect.width + 135
+            diagonal_speed = sqrt(self.dx**2 + self.dy**2)
+            self.dx = diagonal_speed * cos(radians(y))
+            self.dy = -1 * diagonal_speed * sin(radians(y))
+        else:
+            self.rect.top = self.bar.rect.bottom
+            self.dy = -self.dy
 
-        old_rect = self.rect.copy()
-        self.rect.move_ip(self.dx, self.dy)
-        if self.rect.colliderect(self.bar.rect):
-            if self.bar.rect.left >= old_rect.right:
-                self.rect.right = self.bar.rect.left
-                self.dx = -self.dx
-            elif self.bar.rect.right < old_rect.left:
-                self.rect.left = self.bar.rect.right
-                self.dx = -self.dx
-            elif self.bar.rect.top >= old_rect.bottom:
-                self.rect.bottom = self.bar.rect.top
-                x = self.rect.centerx - self.bar.rect.left
-                y = (45 - 135) * x / self.bar.rect.width + 135
-                diagonal_speed = sqrt(self.dx**2 + self.dy**2)
-                self.dx = diagonal_speed * cos(radians(y))
-                self.dy = -1 * diagonal_speed * sin(radians(y))
-            else:
-                self.rect.top = self.bar.rect.bottom
-                self.dy = -self.dy
+    def _handle_wall_collision(self, SCREEN):
         if self.rect.left < SCREEN.left or self.rect.right > SCREEN.right:
             self.dx = -self.dx
         if self.rect.top < SCREEN.top:
             self.dy = -self.dy
         if self.rect.bottom > SCREEN.bottom:
             self.status = "INIT"
+
+    def update(self, SCREEN: pygame.locals.Rect):
+        if self.status == "INIT":
+            self.rect.centerx = self.bar.rect.centerx
+            self.rect.bottom = self.bar.rect.top
+            return
+
+        old_rect = self.rect.copy()
+        self.rect.move_ip(self.dx, self.dy)
+        
+        if self.rect.colliderect(self.bar.rect):
+            self._handle_bar_collision(old_rect)
+            
+        self._handle_wall_collision(SCREEN)
         self.rect.clamp_ip(SCREEN)
 
     def draw(self, screen_surface: pygame.Surface):
